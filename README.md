5bdd4f93b6bf623c8a34d848f79651c72fa0beb4
rhedia <rhedia@gmail.com>
2025-03-23 16:27:56 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Instructions
+Clone the repo under your home directory
+- cd setup-linux-box
+- ./init-linux-box.sh
+
+# What it does
+Installs the required tools
+Setup the conf files for
+- neovim
+- tmux
+- bashrc
+
---
+++ init-linux-box.sh
@@ -0,0 +1,18 @@
+sudo apt update
+sudo apt install -y tmux
+sudo apt install -y neovim
+sudo apt install -y fzf
+sudo apt install -y zoxide
+sudo apt install -y jq
+
+cp .bashrc ~/
+mkdir ~/.config
+mkdir ~/.config/nvim
+cp init.lua ~/.config/nvim
+cp .tmux.conf ~/
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+
