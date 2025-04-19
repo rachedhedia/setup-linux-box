c50e2af8a9202eacba4c010161f725567bf97bfd
Rached HEDIA <37162467+rachedhedia@users.noreply.github.com>
2025-04-19 21:44:45 +0100

    Add pet snippets manager

--- setup-linux-box.sh
+++ setup-linux-box.sh
@@ -1,4 +1,3 @@
-
 cp .bashrc ~/
 sudo apt update
 sudo apt install -y tmux
@@ -6,7 +5,7 @@ sudo apt install -y neovim
 sudo apt install -y btop
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-sudo apt install -y eza 
+sudo apt install -y eza
 sudo apt install -y zoxide
 sudo apt install -y jq
 
@@ -21,6 +20,10 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
+
+curl -Lo pet.tar.gz "https://github.com/knqyf263/pet/releases/download/v1.0.1/pet_1.0.1_linux_386.tar.gz"
+tar xf pet.tar.gz pet
+sudo install pet -D -t /usr/local/bin/
 
 echo 'export PATH="$HOME/.fzf/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
